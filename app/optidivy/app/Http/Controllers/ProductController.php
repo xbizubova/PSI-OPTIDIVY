11ddcc9b52272de87f664bb3e77a28beb5fa44f3
oleksiikhmelevskyi <hexus.jr@gmail.com>
2026-04-26 20:09:06 +0200

    Refactor kategoria method to map product types and handle invalid categories

--- app/optidivy/app/Http/Controllers/ProductController.php
+++ app/optidivy/app/Http/Controllers/ProductController.php
@@ -14,7 +14,14 @@ class ProductController extends Controller
 
     public function kategoria(string $kategoria)
     {
-        $products = Stock::where('product_type', $kategoria)->get();
+        $map = [
+            'okuliare' => 'glasses',
+            'sosovky'  => 'contact_lenses',
+        ];
+
+        abort_unless(isset($map[$kategoria]), 404);
+
+        $products = Stock::where('product_type', $map[$kategoria])->get();
         return view('index', compact('products'));
     }
 }
